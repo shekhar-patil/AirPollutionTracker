8504f3de048407b46ec5ff0a5c3cac47807e3128
ShekharPatil <patilshekhar900@gmail.com>
2023-12-27 14:07:39 +0530

    Update cities_controller.rb
    Code refactor

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,29 +1,39 @@
 class CitiesController < ApplicationController
-  before_action :set_city, only: %i[ show ]
+  before_action :set_city, only: [:show]
 
   def index
     @cities = City.all
-
-    @average_aqi_per_state ||= City.joins(:pollution_data)
-                                   .select("cities.state AS state_name, AVG(pollution_data.index) AS avg_aqi")
-                                   .group("cities.state")
+    @average_aqi_per_state = calculate_average_aqi_per_state
   end
 
   def show
-    @monthly_avg_aqi_per_location ||= City.joins(:pollution_data).where(id: @city)
-                                           .select("EXTRACT(MONTH FROM TO_TIMESTAMP(pollution_data.dt)) AS month, AVG(pollution_data.index) AS avg_aqi")
-                                           .group("EXTRACT(MONTH FROM TO_TIMESTAMP(pollution_data.dt))")
-
-
-    @average_aqi_per_location ||= City.joins(:pollution_data).where(id: @city)
-                                           .select("cities.id, AVG(pollution_data.index) AS avg_aqi")
-                                           .group("cities.id")
-
+    @monthly_avg_aqi_per_location = calculate_monthly_avg_aqi_per_location
+    @average_aqi_per_location = calculate_average_aqi_per_location
   end
 
   private
 
   def set_city
     @city = City.includes(:pollution_data).find(params[:id])
+  end
+
+  def calculate_average_aqi_per_state
+    City.joins(:pollution_data)
+        .select("cities.state AS state_name, AVG(pollution_data.index) AS avg_aqi")
+        .group("cities.state")
+  end
+
+  def calculate_monthly_avg_aqi_per_location
+    City.joins(:pollution_data)
+        .where(id: @city)
+        .select("EXTRACT(MONTH FROM TO_TIMESTAMP(pollution_data.dt)) AS month, AVG(pollution_data.index) AS avg_aqi")
+        .group("EXTRACT(MONTH FROM TO_TIMESTAMP(pollution_data.dt))")
+  end
+
+  def calculate_average_aqi_per_location
+    City.joins(:pollution_data)
+        .where(id: @city)
+        .select("cities.id, AVG(pollution_data.index) AS avg_aqi")
+        .group("cities.id")
   end
 end
